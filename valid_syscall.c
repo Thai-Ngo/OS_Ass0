efae69b57a8e6a1f383ae83c0da883ba09d5f939
Thai Ngo <ngominhhthai2001@gmail.com>
2021-05-12 09:00:57 +0000

    Customize valid_syscall.c

--- valid_syscall.c
+++ valid_syscall.c
@@ -5,18 +5,30 @@
 #include <stdint.h>
 
 int main() {
-	pid_t mypid = getppid();
-	printf("PID: %d\n", mypid);
 	struct procinfos info;
-
-	if (get_proc_info(mypid, &info) == 0) {
+	
+	printf("\nCURRENT PROCESS INFORMATION\n\n");
+	if (get_proc_info(-1, &info) == 0) {
 		printf("My student ID: %ld\n", info.studentID);
 		printf("My PID: %d, name: %s\n", info.proc.pid, info.proc.name);
-		printf("Parent PID: %d, name %s\n", info.parent_proc.pid, info.parent_proc.name);
-		printf("Child PID: %d, name %s\n", info.oldest_child_proc.pid, info.oldest_child_proc.name);
-	} else {
-		printf("Cannot get information from the process %d\n", mypid);
+		printf("My parent PID: %d, name: %s\n", info.parent_proc.pid, info.parent_proc.name);
+		printf("My oldest child PID: %d, name: %s\n", info.oldest_child_proc.pid, info.oldest_child_proc.name);
 	}
+       	else {
+		printf("Cannot get information from the process %d\n", info.proc.pid);
+	}
+
+	printf("\n\nPARENT PROCESS INFORMATION\n\n");
+	if (get_proc_info(info.parent_proc.pid, &info) == 0) {
+		printf("My student ID: %ld\n", info.studentID);
+		printf("My PID: %d, name: %s\n", info.proc.pid, info.proc.name);
+		printf("My parent PID: %d, name: %s\n", info.parent_proc.pid, info.parent_proc.name);
+		printf("My oldest child PID: %d, name: %s\n", info.oldest_child_proc.pid, info.oldest_child_proc.name);
+	}
+	else {
+		printf("Cannot get information from the process %d\n", info.proc.pid);
+	}
+
 	return 0;
 }
 
